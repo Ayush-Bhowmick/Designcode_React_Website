82201ecf5c17577bdc38b0530de077fad5d2bb39
AyushBhowmick <ayushbhowmick2000@gmail.com>
2019-10-23 23:59:15 +0530

    Interactions and Animations

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ const IndexPage = () => (
   <div>
     <div className="Hero">
       <div className="HeroGroup">
-        <h1>Learn to design and Code React Apps</h1>
+        <h1>Learn to <br /> design and Code React Apps</h1>
         <p>Complete courses about the best tools and design systems. Protoype and build apps with React and Swift.</p>
         <Link to="/page-2/">Watch the Video</Link>
       </div>
