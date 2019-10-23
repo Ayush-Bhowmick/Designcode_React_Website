81e96768094012130b0e817d46d69bc3317c0a9d
AyushBhowmick <ayushbhowmick2000@gmail.com>
2019-10-23 20:41:37 +0530

    INITIAL COMMIT

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,8 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Learn to design and Code React Apps</h1>
+    <p>Complete courses.</p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
